4e9ce722c33fc18c32938eac7af7b9cfca96428c
root <root@vps36084.oxide.systems>
2020-11-22 12:36:08 -0500

    some more stuff probably i cant even remember just help me god damn it

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const server = new sv(app)
 const io = require("socket.io")(server);
 const { v4: roomID } = require("uuid");
 const cookieParser = require("cookie-parser");
-
+const rooms = new Map();
 
 // set up and run server
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(cookieParser(config.cookie_parser));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     const userID = roomID();
@@ -24,14 +25,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/new", (req, res) => {
-    const id = roomID()
-    res.cookie("roomID", id).redirect(`/${id}`)
+    const id = roomID();
+    rooms.set(id, [req.cookies.userID]);
+    res.cookie("roomID", id).redirect(`/${id}`);
 });
 
 app.get("/:room", (req, res) => {
-    res.render("room", { roomID: req.cookies.roomID, userID: req.cookies.userID });
+    res.render("room", { roomID: req.cookies.roomID || req.params.room, userID: req.cookies.userID });
 });
 
+app.post("/join", (req, res) => {
+    const postInfo = req.body;
+    const getId = rooms.get(postInfo.roomID);
+    if (getId) {
+        return res.cookie("roomID", postInfo.roomID).redirect(`/${postInfo.roomID}`);
+    } else {
+        return res.redirect("/");
+    }
+});
 
 // socket.io listeners
 
